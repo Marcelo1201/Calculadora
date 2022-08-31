b2112effb04a89a6a6f798cf4d9a6b1e83a206a7
Marcelo Giacometti <marcelo.giacomettigemeo1@gmail.com>
2022-08-31 19:53:54 -0300

    putting the JavaScript

---
+++ JS.js
@@ -0,0 +1,8 @@
+function numero(num){
+    var number = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = number + num
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = "";
+}
