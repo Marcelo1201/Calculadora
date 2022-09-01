1f8e466d311e8c339ee081b333383f74196071ef
Marcelo Giacometti <marcelo.giacomettigemeo1@gmail.com>
2022-09-01 17:26:52 -0300

    Finishing the project

--- JS.js
+++ JS.js
@@ -1,8 +1,20 @@
-function numero(num){
-    var number = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML = number + num
+function inserir(num){
+    var number = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = number + num;
 }
 
 function limpar(){
     document.getElementById('resultado').innerHTML = "";
+}
+
+function apagar(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+       document.getElementById('resultado').innerHTML = eval(resultado);
+    }
 }
